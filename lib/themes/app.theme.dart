0032f9b27cd0def3c6fb37804eabdb6c185347fc
Bernardo Veras <bernardo@vvssistemas.com.br>
2020-12-02 09:13:04 -0300

    02-12: Add themes e fontes

---
+++ lib/themes/app.theme.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.light;
+const primaryColor = const Color(0xFF6C0DDB);
+const accentColor = const Color(0xFF000000);
+const greyColor = const Color(0xFF444444);
+const lightPinkColor = const Color(0xFFCBC2D6);
+
+ThemeData appTheme() {
+  return ThemeData(
+    brightness: brightness,
+    textTheme: new TextTheme(
+      button: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      caption: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      display1: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      display2: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      display3: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      display4: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      headline: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      overline: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      subhead: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      subtitle: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+      title: TextStyle(
+        fontFamily: "InterRegular",
+      ),
+    ),
+    primaryColor: primaryColor,
+    accentColor: accentColor,
+    scaffoldBackgroundColor: Color(0xFFFAFAFA),
+    splashColor: Colors.transparent,
+    highlightColor: Colors.transparent,
+  );
+}
