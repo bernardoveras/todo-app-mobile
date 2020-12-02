58d8321704911634bc48eb465a04cfce5ebf951a
Bernardo Veras <bernardo@vvssistemas.com.br>
2020-12-02 02:50:16 -0300

    02-12: Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo App
+
+- [Projeto completo](https://github.com/bernardoveras/todo-app)
---
+++ lib/app/app_module.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/app/app_widget.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRouter> get routers => [
+        // ModularRouter(Modular.initialRoute, module: HomeModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
